4877ce86b2604856cef83de69a5fd61af6ae9450
Shaik Khaja Mohiddin <shaik_k@worksap.co.jp>
2019-04-25 19:16:48 +0900

    remove surrounding async block

--- src/index.ts
+++ src/index.ts
@@ -11,45 +11,43 @@ const getCDataJson = (body: string): Root => {
     return JSON.parse(cDataContent.substring(cDataContent.indexOf("{"), cDataContent.lastIndexOf("}") + 1));
 }
 
-(async () => {
+const scrape = new Scrape<URLTypes, DataTypes, Post | User>(
+    {
+        tag: (url, body) => {
+            const data = getCDataJson(body)
+            const urls = data.archiveIndex.yearlyBuckets.filter(b => b.hasStories)
+                .map(b => ({ type: "year" as const, url: url + b.year + "/" }))
 
-    const scrape = new Scrape<URLTypes, DataTypes, Post | User>(
-        {
-            tag: (url, body) => {
-                const data = getCDataJson(body)
-                const urls = data.archiveIndex.yearlyBuckets.filter(b => b.hasStories)
-                    .map(b => ({ type: "year" as const, url: url + b.year + "/" }))
-
-                return { data: [], urls };
-            },
-            year: (url, body) => {
-                const data = getCDataJson(body)
-                const urls = data.archiveIndex.monthlyBuckets.filter(b => b.hasStories)
-                    .map(b => ({ type: "month" as const, url: url + b.month + "/" }))
-                return { data: [], urls };
-            },
-            month: (url, body) => {
-                const data = getCDataJson(body)
-                const urls = data.archiveIndex.dailyBuckets.filter(b => b.hasStories)
-                    .map(b => ({ type: "day" as const, url: url + b.day + "/" }))
-                return { data: [], urls };
-            },
-            day: (url, body) => {
-                const data: Root = getCDataJson(body)
-                const users = Object.values(data.references.User).filter(u => u.type === "User").map(({ userId, username, name, bio, createdAt, imageId }) => ({ userId, username, name, bio, createdAt, imageId }))
-                const posts = Object.values(data.references.Post).filter(p => p.type === "Post" && p.detectedLanguage === "en")
-                    .map(({ id, creatorId, title, slug, firstPublishedAt, previewContent }) =>
-                        ({ id, creatorId, title, slug, createdAt: firstPublishedAt, content: previewContent.bodyModel.paragraphs.map(p => p.text).join(" ") }))
-                return {
-                    data: [
-                        ...users.map(u => ({ type: 'user' as const, data: u, key: u.userId })),
-                        ...posts.map(p => ({ type: 'post_summary' as const, data: p, key: p.id }))
-                    ],
-                    urls: []
-                };
-            }
+            return { data: [], urls };
         },
-        10
-    );
-    await scrape.startScraping({ type: 'tag', url: "https://medium.com/tag/javascript/archive/" })
-})()
+        year: (url, body) => {
+            const data = getCDataJson(body)
+            const urls = data.archiveIndex.monthlyBuckets.filter(b => b.hasStories)
+                .map(b => ({ type: "month" as const, url: url + b.month + "/" }))
+            return { data: [], urls };
+        },
+        month: (url, body) => {
+            const data = getCDataJson(body)
+            const urls = data.archiveIndex.dailyBuckets.filter(b => b.hasStories)
+                .map(b => ({ type: "day" as const, url: url + b.day + "/" }))
+            return { data: [], urls };
+        },
+        day: (url, body) => {
+            const data: Root = getCDataJson(body)
+            const users = Object.values(data.references.User).filter(u => u.type === "User").map(({ userId, username, name, bio, createdAt, imageId }) => ({ userId, username, name, bio, createdAt, imageId }))
+            const posts = Object.values(data.references.Post).filter(p => p.type === "Post" && p.detectedLanguage === "en")
+                .map(({ id, creatorId, title, slug, firstPublishedAt, previewContent }) =>
+                    ({ id, creatorId, title, slug, createdAt: firstPublishedAt, content: previewContent.bodyModel.paragraphs.map(p => p.text).join(" ") }))
+            return {
+                data: [
+                    ...users.map(u => ({ type: 'user' as const, data: u, key: u.userId })),
+                    ...posts.map(p => ({ type: 'post_summary' as const, data: p, key: p.id }))
+                ],
+                urls: []
+            };
+        }
+    },
+    10
+);
+
+scrape.startScraping({ type: 'tag', url: "https://medium.com/tag/javascript/archive/" })
